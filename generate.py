7a12f6e093f26962558ec5c69b72e231a944b788
Mark Baggett <mbagget1@utk.edu>
2019-01-04 17:11:38 -0500

    Generate initial docs.

--- generate.py
+++ generate.py
@@ -15,10 +15,56 @@ class DLTN:
                 provider_rst_file.write(f'{provider["name"]}\n')
                 provider_rst_file.write("=" * len(provider["name"]))
                 provider_rst_file.write(f"\n\nDetails\n-------\n\n")
+                provider_rst_file.write(self.__generate_details_section(provider))
+                provider_rst_file.write(f"\n\nDatasets\n--------\n\n")
                 provider_rst_file.write(
-                    f"\nEmail: {provider['email']}\n\nDescription: {provider['description']}\n\nOAI endpoint: "
-                    f"{provider['homepage']}"
+                    self.__generate_dataset_details_section(provider["id"])
                 )
+
+    @staticmethod
+    def __generate_details_section(current_provider):
+        total_records = repox_connection.count_records_from_provider(
+            current_provider["id"]
+        )
+        return (
+            f"\nEmail: {current_provider['email']}\n\nDescription: {current_provider['description']}\n\nOAI "
+            f"endpoint: {current_provider['homepage']}\n\nTotal records: {total_records}"
+        )
+
+    @staticmethod
+    def __generate_dataset_details_section(provider_id):
+        dataset_details = ""
+        for dataset in repox_connection.get_list_of_sets_from_provider(
+            provider_id, True
+        ):
+            current_set = DataSet(dataset["dataSource"]["id"])
+            dataset_details += f"{current_set.generate_rst_for_set()}\n\n"
+        return dataset_details
+
+
+class DataSet:
+    def __init__(self, set_id):
+        self.details = repox_connection.get_dataset_details(set_id)
+        self.total_records = repox_connection.count_records_in_dataset(set_id)
+        self.last_ingest_date = repox_connection.get_last_ingest_date_of_set(set_id)
+
+    # TODO Add OAI Record Counter
+    def generate_rst_for_set(self):
+        dataset_details = (
+            f"{self.details['name']}\n\n* **Record Total**: {self.total_records}\n"
+            f"* **Metadata format**: {self.details['dataSource']['metadataFormat']}\n"
+            f"* **Last ingest date**: {self.last_ingest_date}\n"
+            f"* **Dataset Type**: {self.details['dataSource']['dataSetType']}\n"
+        )
+        if self.details["dataSource"]["dataSetType"] == "DIR":
+            dataset_details += f"* **Source Directory**: {self.details['dataSource']['sourcesDirPath']}\n\n"
+        elif self.details["dataSource"]["dataSetType"] == "OAI":
+            dataset_details += (
+                f"* **OAI Endpoint**: {self.details['dataSource']['oaiSourceURL']}\n"
+                f"* **OAI Set**: {self.details['dataSource']['oaiSet']}\n"
+                f"* **OAI MODS Records**: \n\n"
+            )
+        return dataset_details
 
 
 if __name__ == "__main__":
