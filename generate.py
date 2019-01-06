7759165bbc0efb4a9cd6bc9c57fbeae436f986c8
Mark Baggett <mbagget1@utk.edu>
2019-01-06 03:53:28 -0500

    Add optional MODS record counter.

--- generate.py
+++ generate.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from emoji import emojize
 import requests
 import arrow
+from lxml import etree
 
 settings = yaml.safe_load(open("config.yml", "r"))
 repox_connection = Repox(settings["repox"], settings["username"], settings["password"])
@@ -11,6 +12,7 @@ in_dpla = requests.get(
     "https://raw.githubusercontent.com/dpla/ingestion/develop/profiles/tn.pjs"
 ).json()["sets"]
 now = arrow.utcnow().to("US/Eastern").format("YYYY-MM-DD")
+generate_total_mods = settings["generate_mods_count"]
 
 
 class DLTN:
@@ -65,7 +67,6 @@ class DataSet:
         self.last_ingest_date = repox_connection.get_last_ingest_date_of_set(set_id)
         self.in_dpla = self.__in_dpla(set_id)
 
-    # TODO Add OAI Record Counter
     def generate_rst_for_set(self):
         dataset_details = (
             f"{self.details['name']}\n\n* **Record Total**: {self.total_records}\n"
@@ -74,22 +75,56 @@ class DataSet:
             f"* **Last ingest date**: {self.last_ingest_date}\n"
             f"* **Dataset Type**: {self.details['dataSource']['dataSetType']}\n"
         )
+        total_mods = 0
+        if self.in_dpla is True and generate_total_mods is True:
+            counter = OAIMODSCounter(self.details['dataSource']['id'])
+            counter.list_records()
+            total_mods = counter.total_records
         if self.details["dataSource"]["dataSetType"] == "DIR":
             dataset_details += f"* **Source Directory**: {self.details['dataSource']['sourcesDirPath']}\n\n"
         elif self.details["dataSource"]["dataSetType"] == "OAI":
             dataset_details += (
                 f"* **OAI Endpoint**: {self.details['dataSource']['oaiSourceURL']}\n"
                 f"* **OAI Set**: {self.details['dataSource']['oaiSet']}\n"
-                f"* **OAI MODS Records**: \n\n"
+                f"* **OAI MODS Records**: {total_mods}\n\n"
             )
         return dataset_details
 
     @staticmethod
-    def __in_dpla(id):
-        if id in in_dpla:
+    def __in_dpla(identifier):
+        if identifier in in_dpla:
             return True
         else:
             return False
+
+
+class OAIMODSCounter:
+    def __init__(self, set_name):
+        self.set_name = set_name
+        self.oai_base = "http://dpla.lib.utk.edu/repox/OAIHandler"
+        self.endpoint = f"{self.oai_base}?verb=ListRecords&metadataPrefix=MODS&set={set_name}"
+        self.token = ""
+        self.status = "In Progress"
+        self.total_records = 0
+
+    def process_token(self, token):
+        if len(token) == 1:
+            self.token = f'&resumptionToken={token[0].text}'
+            return
+        else:
+            self.status = "Done"
+            return
+
+    def list_records(self):
+        r = requests.get(f"{self.endpoint}")
+        oai_document = etree.fromstring(r.content)
+        self.process_token(oai_document.findall('.//{http://www.openarchives.org/OAI/2.0/}resumptionToken'))
+        self.total_records += len(oai_document.findall('.//{http://www.openarchives.org/OAI/2.0/}metadata'))
+        if self.status is not "Done":
+            self.endpoint = f"{self.oai_base}?verb=ListRecords{self.token}"
+            return self.list_records()
+        else:
+            return
 
 
 if __name__ == "__main__":
